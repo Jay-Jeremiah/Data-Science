0800e6ee7c9d97d9b7dcb322413a43da59a15c45
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-07 10:02:34 +0300

    data science intro

---
+++ Functions/Functions.py
@@ -0,0 +1,12 @@
+# In data science, we have 3 common functions namely:
+# max(), min(), mean()
+
+# max()
+
+Cw_marks = max(56,78,32,98,12,10) # runs the highest value in this array
+print(Cw_marks)
+
+Cw_marks1 = min(6,78,32,98,12,10) # runs the lowest value in the list
+print(Cw_marks1)
+
+
---
+++ Functions/mean.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+calorie_burnage = [240, 250, 260, 290, 300, 320]
+
+mn = np.mean(calorie_burnage)
+
+print("The mean is: ", mn)
